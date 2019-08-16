95e577478d62e3514713164a8161dc0e417b0a97
pyphilia <pyphilia@gmail.com>
2019-08-16 14:46:17 +0200

    build: init project

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "bootstrap";
+import "./styles/main.scss";
+import "./index.html";
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+// This library allows us to combine paths easily
+const path = require('path');
+
+const DIST = 'public';
+
+module.exports = {
+   mode: 'development',
+   devtool: 'inline-source-map',
+   entry: path.join(__dirname, 'src/index.js'),
+   output: {
+      path: path.resolve(__dirname, DIST),
+      filename: 'main.js'
+   },
+   resolve: {
+      extensions: ['.js']
+   },
+   module: {
+      rules: [
+         {
+            test: /\.js(x*)/,
+            use: {
+               loader: 'babel-loader',
+               options: {
+                  "presets": ["@babel/env", "@babel/react"]
+               }
+            }
+         },
+         {
+            test: /\.(s*)css$/,
+            
+            use: [
+               {
+                  loader: 'style-loader',
+               },
+               {
+                  loader: 'css-loader',
+                  options: {
+                     sourceMap: true,
+                  },
+               },
+               {
+                  loader: 'sass-loader',
+                  options: {
+                     sourceMap: true,
+                  },
+               },
+            ],
+         },
+         {
+            test: /\.html$/,
+            use: ['file-loader?name=[name].[ext]', 'extract-loader', 'html-loader'],
+          }
+      ]
+   },
+   devServer: {
+      contentBase: DIST,
+      hot: true,
+      inline: true,
+      watchContentBase: true,
+   }
+};
